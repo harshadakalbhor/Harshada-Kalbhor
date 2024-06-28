7fcda3cc6bd6c59471d543a2f00d4367605888bf
harsh <harsh@Harshadakalbhor>
2024-06-28 16:12:48 +0530

    Eligibility Criteria

---
+++ Eligibility/src/eligibility/Eligibility.java
@@ -0,0 +1,16 @@
+package eligibility;
+
+public class Eligibility {
+
+	public static void main(String[] args) {
+		int age=20;
+		if(age>18) {
+			System.out.println("Eligible for voting");
+		}
+		else {
+			System.out.println("Not Eligible for voting");
+		}
+
+	}
+
+}
