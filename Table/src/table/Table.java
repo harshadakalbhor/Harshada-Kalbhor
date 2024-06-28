e8f774c9bb4fbb49ee87b6a2777a1ed0865d13dd
harsh <harsh@Harshadakalbhor>
2024-06-28 16:49:47 +0530

    Table,Natural Numbers

---
+++ Table/src/table/Table.java
@@ -0,0 +1,29 @@
+package table;
+
+public class Table {
+
+	public static void main(String[] args) {
+		
+		int a=2;
+		
+		int b=1;
+		
+		//To print the Table using "for loop"
+		
+		for(int i=1;i<=10;i++) {
+			System.out.println(a*i);
+		}
+		
+		
+		//Print Natural Numbers using "do while  loop"
+		do {
+			System.out.println(b++);
+		}
+		while(b<=10);
+		
+		
+		
+
+	}
+
+}
