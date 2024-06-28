6f2c9665dab42efaf87e488a2e05781d0abe43cf
harsh <harsh@Harshadakalbhor>
2024-06-28 16:17:00 +0530

    Loop

---
+++ Loop/src/loop/Loop.java
@@ -0,0 +1,30 @@
+package loop;
+
+public class Loop {
+
+	public static void main(String[] args) {
+			
+				
+				int x=0, y=7;
+				int a=10;
+				int b=11;
+				
+				if (x>=y) {
+					System.out.println("true");
+					}else {
+						System.out.println("False")
+						;
+					}
+				if (! (a<b ) || (a==b)){
+					System.out.println("condition is true");
+					
+				} else {
+					System.out.println("Condition is false");
+				}
+				// TODO Auto-generated method stub
+
+			}
+		
+}
+
+
