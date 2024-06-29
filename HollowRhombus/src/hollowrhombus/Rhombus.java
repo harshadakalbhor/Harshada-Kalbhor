8fdc43939f24fcf74beeeae39fdce44005e66de8
harsh <harsh@Harshadakalbhor>
2024-06-29 13:48:04 +0530

    Hollow Rhombus

---
+++ HollowRhombus/src/hollowrhombus/Rhombus.java
@@ -0,0 +1,41 @@
+package hollowrhombus;
+
+public class Rhombus {
+
+	public static void main(String[] args) {
+		
+		        int n=5;
+		        
+		        //Outer loop
+		        for(int i=1;i<=n;i++){
+		            // spacing
+		            for(int j=1;j<=n-i;j++){
+		                System.out.print(" ");
+		            }
+		            if(i==1 || i==n)
+		            for(int j=1;j<=n;j++)
+		     
+		             {
+		                    System.out.print("*");
+		             }
+		        else
+		{
+		                for(int j=1;j<=n;j++)
+		     
+		                {  
+		                         if(j==1 || j==n)
+		                        System.out.print("*");
+		                       else
+		                  
+		                                    System.out.print(" ");
+		                        }
+		          }
+		            System.out.println();
+		        }
+		    }
+
+		
+
+	}
+
+
